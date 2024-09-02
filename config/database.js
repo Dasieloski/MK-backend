d4a2271301a1309e5eb8fcde92e23aaa6e18a954
Dasiel <dasieltorres99@gmail.com>
2024-09-02 18:54:52 -0400

    Mensaje descriptivo sobre los cambios

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true),  // Asegúrate de que SSL esté activado
+      ssl: env.bool('DATABASE_SSL', true), // Asegúrate de que SSL esté activado
     },
   },
 });
