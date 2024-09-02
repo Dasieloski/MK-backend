c66c7c188f6dc94cfe4229a88fffc7fe09fe9b77
Dasiel <dasieltorres99@gmail.com>
2024-09-02 19:40:45 -0400

    Descripción de los cambios realizados

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,24 @@
+// config/plugins.js
+
 module.exports = {
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: process.env.CLOUDINARY_NAME,
-        api_key: process.env.CLOUDINARY_KEY,
-        api_secret: process.env.CLOUDINARY_SECRET,
-      },
+    // Configuración del plugin de Cloudinary
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: process.env.CLOUDINARY_NAME,
+                api_key: process.env.CLOUDINARY_KEY,
+                api_secret: process.env.CLOUDINARY_SECRET,
+            },
+        },
     },
-  },
+
+    // Configuración del plugin `strapi-neon-tech-db-branches`
+    'strapi-neon-tech-db-branches': {
+        enabled: true,
+        config: {
+            branchName: process.env.BRANCH_NAME, // Debe coincidir con 'nueva-rama'
+            // Otros parámetros de configuración si los hay
+        },
+    },
 };
