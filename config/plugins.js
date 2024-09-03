e74bd34881df315aa7f22f1ddf89291944631bf3
Dasiel <dasieltorres99@gmail.com>
2024-09-03 01:28:51 -0400

    Instala cloudinary y corrige configuración

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,16 @@
-module.exports = {
-    upload: {
-        config: {
-            provider: 'cloudinary',
-            providerOptions: {
-                cloud_name: process.env.CLOUDINARY_NAME,
-                api_key: process.env.CLOUDINARY_KEY,
-                api_secret: process.env.CLOUDINARY_SECRET,
-            },
-        },
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
-}
+  },
+});
