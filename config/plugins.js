2ef5c1fe4219993bdf0d25fe9413cdf7d27b68c6
Dasiel <dasieltorres99@gmail.com>
2024-09-02 20:24:51 -0400

    Descripción de los cambios realizados

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,4 @@
-// config/plugins.js
-
 module.exports = {
-    // Configuración del plugin de Cloudinary
     upload: {
         config: {
             provider: 'cloudinary',
@@ -13,15 +10,13 @@ module.exports = {
         },
     },
 
-    // Configuración del plugin `strapi-neon-tech-db-branches`
     'strapi-neon-tech-db-branches': {
         enabled: true,
         config: {
-            branchName: process.env.BRANCH_NAME || 'nueva-rama', // Default branch name if env var is missing
-            neonApiKey: "zg0zxpnuj2emhun5t3n8s24pm3b1qjft2myi239u28hqa9wboz6csb0dej1ax5kc", // get it from here: https://console.neon.tech/app/settings/api-keys
-            neonProjectName: "mk-tattoo", // the neon project under wich your DB runs
-            neonRole: "mktattoodb_owner", // create it manually under roles for your project first
-            // Otros parámetros de configuración si los hay
+            branchName: process.env.BRANCH_NAME || 'nueva-rama', // Usar la variable BRANCH_NAME
+            neonApiKey: process.env.NEON_API_KEY, // Usar la variable de entorno NEON_API_KEY
+            neonProjectName: process.env.NEON_PROJECT_NAME, // Usar la variable de entorno NEON_PROJECT_NAME
+            neonRole: process.env.NEON_ROLE, // Usar la variable de entorno NEON_ROLE
         },
     },
 };
