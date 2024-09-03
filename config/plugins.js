860fc6e91f57211948c2cae0f2cc27bad22bf68f
Dasiel <dasieltorres99@gmail.com>
2024-09-03 00:11:55 -0400

    cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = {
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: process.env.CLOUDINARY_NAME,
+                api_key: process.env.CLOUDINARY_KEY,
+                api_secret: process.env.CLOUDINARY_SECRET,
+            },
+        },
+    },
+}
