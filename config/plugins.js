3a5459cc159e67bc67d596c479b7ac1425fb71d7
Dasiel <dasieltorres99@gmail.com>
2024-09-02 20:09:01 -0400

    Descripción de los cambios realizados

--- config/plugins.js
+++ config/plugins.js
@@ -12,4 +12,16 @@ module.exports = {
             },
         },
     },
-}
+
+    // Configuración del plugin `strapi-neon-tech-db-branches`
+    'strapi-neon-tech-db-branches': {
+        enabled: true,
+        config: {
+            branchName: process.env.BRANCH_NAME || 'nueva-rama', // Default branch name if env var is missing
+            neonApiKey: "zg0zxpnuj2emhun5t3n8s24pm3b1qjft2myi239u28hqa9wboz6csb0dej1ax5kc", // get it from here: https://console.neon.tech/app/settings/api-keys
+            neonProjectName: "mk-tattoo", // the neon project under wich your DB runs
+            neonRole: "mktattoodb_owner", // create it manually under roles for your project first
+            // Otros parámetros de configuración si los hay
+        },
+    },
+};
